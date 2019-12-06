c36ad06e5c2b5e7fd503d613af03a2a859bd6481
喻聪 <yucongcong@i-syp.com>
2019-12-07 00:21:27 +0800

    功能完善

--- dubbo-provider/src/main/java/com/dc/sb/dubbo/provider/DubboProviderApplication.java
+++ dubbo-provider/src/main/java/com/dc/sb/dubbo/provider/DubboProviderApplication.java
@@ -1,11 +1,13 @@
 package com.dc.sb.dubbo.provider;
 
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+//import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableDubboConfiguration
+//@EnableDubboConfiguration
+@EnableDubbo
 public class DubboProviderApplication {
 
 	public static void main(String[] args) {
